40b43752d69779a39ccf05929710b04543fad2f2
wwwal2 <smalex9@gmail.com>
2020-01-17 12:31:22 +0200

    configure eslint: checked airbnb rules, checked proptypes rules

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Header(props) {
-	const { name } = props;
-	return <div>
-		hi {name}
-	</div>;
-
+  const { name } = props;
+  return (
+    <div>
+      hi
+      {name}
+    </div>
+  );
 }
 
 Header.propTypes = {
-	name: PropTypes.string
+  name: PropTypes.string,
+};
+
+Header.defaultProps = {
+  name: 'al2',
 };
