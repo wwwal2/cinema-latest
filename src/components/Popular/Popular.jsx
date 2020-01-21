7bbe61e9064e33b6f337da822cfd51762bec3d92
Oleksii Smirnov <oleksiis@default-value.com>
2020-01-21 16:56:56 +0200

    set page routes, link pages to routes

---
+++ src/components/Popular/Popular.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Popular</h1>;
+  }
+}
