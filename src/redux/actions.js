652f748c709682e8acca771e7e38f0ad33720cd4
Oleksii Smirnov <oleksiis@default-value.com>
2020-01-23 13:48:00 +0200

    add basic pattern for rating and genre filters

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,13 @@
 const test = (payload) => ({ type: 'TEST', payload });
 const test2 = (payload) => ({ type: 'TEST2', payload });
+const addRating = (payload) => ({ type: 'ADDRATING', payload });
+const addGenre = (payload) => ({ type: 'ADDGENRE', payload });
+const addYear = (payload) => ({ type: 'ADDYEAR', payload });
 
 export {
   test,
   test2,
+  addRating,
+  addGenre,
+  addYear,
 };
