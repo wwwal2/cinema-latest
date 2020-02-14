75632601f8494025ea8c131b68bf0945c622d3e7
Oleksii Smirnov <oleksiis@default-value.com>
2020-02-14 14:54:31 +0200

    add mobile version header

--- src/App.jsx
+++ src/App.jsx
@@ -31,9 +31,6 @@ function App(props) {
         </Switch>
         <Pagination />
         <div className={style.buffer} />
-        <div className={style.pusher}>
-          <div className={style.pusherChild} />
-        </div>
       </div>
       <Footer />
     </Router>
