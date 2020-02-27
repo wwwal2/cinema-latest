aea19208758fe06866d89bae8725bed43348d4e4
Oleksii Smirnov <oleksiis@default-value.com>
2020-02-27 18:58:41 +0200

    replace pageNotFound Image

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import missingPage from '../../../images/missingPage2.png';
+import missingPage from '../../../images/missingPage.gif';
 import styles from './PageNotFound.scss';
 
 export default function Footer() {
   return (
     <section className={styles.container}>
-      <h1>
-        PAGE NOT FOUND
-      </h1>
-      <img alt="missing page image" src={missingPage} className={styles.missingPage} />
+      <img alt="missing page image" src={missingPage} className={styles.image} />
     </section>
   );
 }
